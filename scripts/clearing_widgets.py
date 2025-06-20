477c7323d037a25aded7669597ec5dbb760112c4
16aurora <gursmeep5813@gmail.com>
2025-06-20 10:29:08 +0530

    uploaded the wrong file earlier

--- scripts/clearing_widgets.py
+++ scripts/clearing_widgets.py
@@ -1,10 +1,10 @@
 import nbformat
 
-with open('../RAG_T5/RAG_without_fine_tuning.ipynb', encoding='utf-8') as f:
+with open('../T5_fine_tuning/t5_fine_tuning.ipynb', encoding='utf-8') as f:
     nb = nbformat.read(f, as_version=4)
 
 if 'widgets' in nb.metadata:
     nb.metadata['widgets'] = {} 
 
-with open('../RAG_T5/RAG_without_fine_tuning.ipynb', 'w', encoding='utf-8') as f:
+with open('../T5_fine_tuning/t5_fine_tuning.ipynb', 'w', encoding='utf-8') as f:
     nbformat.write(nb, f)
